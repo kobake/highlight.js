90200c0b80b2a7dfa851e035c9a87738542ab18a
kobake <kobake@users.sourceforge.net>
2016-10-18 01:35:21 +0900

    途中

--- src/languages/cshtml.js
+++ src/languages/cshtml.js
@@ -37,87 +37,49 @@ function(hljs) {
     ]
   };
 
+  function recursiveParen(begin, end) {
+    var
+    contains = [{begin: begin, end: end}];
+    contains[0].contains = contains;
+    return contains;
+  }
+  function createBlock(def) {
+    return {
+      begin: def.begin, end: def.end,
+      className: def.className,
+      endsParent: def.endsParent,
+      returnBegin: true,
+      contains: [
+        {
+          begin: def.begin,
+          className: def.beginClassName,
+        },
+        {
+          begin: def.end,
+          className: def.endClassName,
+          endsParent: true,
+        },
+        {
+          end: def.end,
+          returnEnd: true,
+          contains: def.contains
+        }
+      ]
+    };
+  }
   // ---------------------------------------- //
-  // C#
+  // cshtml
   // ---------------------------------------- //
-  var KEYWORDS = {
-    keyword:
-      // Normal keywords.
-      'abstract as base bool break byte case catch char checked const continue decimal ' +
-      'default delegate do double else enum event explicit extern finally fixed float ' +
-      'for foreach goto if implicit in int interface internal is lock long ' +
-      'object operator out override params private protected public readonly ref sbyte ' +
-      'sealed short sizeof stackalloc static string struct switch this try typeof ' +
-      'uint ulong unchecked unsafe ushort using virtual void volatile while ' +
-      'nameof ' +
-      // Contextual keywords.
-      'add alias ascending async await by descending dynamic equals from get global group into join ' +
-      'let on orderby partial remove select set value var where yield',
-    literal:
-      'null false true'
-  };
-
-  var VERBATIM_STRING = {
-    className: 'string',
-    begin: '@"', end: '"',
-    contains: [{begin: '""'}]
-  };
-  var VERBATIM_STRING_NO_LF = hljs.inherit(VERBATIM_STRING, {illegal: /\n/});
-  var SUBST = {
-    className: 'subst',
-    begin: '{', end: '}',
-    keywords: KEYWORDS
-  };
-  var SUBST_NO_LF = hljs.inherit(SUBST, {illegal: /\n/});
-  var INTERPOLATED_STRING = {
-    className: 'string',
-    begin: /\$"/, end: '"',
-    illegal: /\n/,
-    contains: [{begin: '{{'}, {begin: '}}'}, hljs.BACKSLASH_ESCAPE, SUBST_NO_LF]
-  };
-  var INTERPOLATED_VERBATIM_STRING = {
-    className: 'string',
-    begin: /\$@"/, end: '"',
-    contains: [{begin: '{{'}, {begin: '}}'}, {begin: '""'}, SUBST]
-  };
-  var INTERPOLATED_VERBATIM_STRING_NO_LF = hljs.inherit(INTERPOLATED_VERBATIM_STRING, {
-    illegal: /\n/,
-    contains: [{begin: '{{'}, {begin: '}}'}, {begin: '""'}, SUBST_NO_LF]
-  });
-  SUBST.contains = [
-    INTERPOLATED_VERBATIM_STRING,
-    INTERPOLATED_STRING,
-    VERBATIM_STRING,
-    hljs.APOS_STRING_MODE,
-    hljs.QUOTE_STRING_MODE,
-    hljs.C_NUMBER_MODE,
-    hljs.C_BLOCK_COMMENT_MODE
-  ];
-  SUBST_NO_LF.contains = [
-    INTERPOLATED_VERBATIM_STRING_NO_LF,
-    INTERPOLATED_STRING,
-    VERBATIM_STRING_NO_LF,
-    hljs.APOS_STRING_MODE,
-    hljs.QUOTE_STRING_MODE,
-    hljs.C_NUMBER_MODE,
-    hljs.inherit(hljs.C_BLOCK_COMMENT_MODE, {illegal: /\n/})
-  ];
-  var STRING = {
-    variants: [
-      INTERPOLATED_VERBATIM_STRING,
-      INTERPOLATED_STRING,
-      VERBATIM_STRING,
-      hljs.APOS_STRING_MODE,
-      hljs.QUOTE_STRING_MODE
-    ]
-  };
-
-  var TYPE_IDENT_RE = hljs.IDENT_RE + '(<' + hljs.IDENT_RE + '(\\s*,\\s*' + hljs.IDENT_RE + ')*>)?(\\[\\])?';
   return {
-    // case_insensitive: true, //?????????
-    keywords: KEYWORDS,
-    illegal: /::/,
+    case_insensitive: true,
     contains: [
+      // XML部
+      {
+        className: 'meta',
+        begin: '<!DOCTYPE', end: '>',
+        relevance: 10,
+        contains: [{begin: '\\[', end: '\\]'}]
+      },
       hljs.COMMENT(
         '<!--',
         '-->',
@@ -125,38 +87,10 @@ function(hljs) {
           relevance: 10
         }
       ),
-      hljs.COMMENT(
-        '@{',
-        '}',
-        {
-          relevance: 10
-        }
-      ),
-      hljs.COMMENT('{\\*', '\\*}'), // C_BLOCK_COMMENT_MODE の真似
-      hljs.COMMENT(
-        '///',
-        '$',
-        {
-          returnBegin: true,
-          contains: [
-            {
-              className: 'doctag',
-              variants: [
-                {
-                  begin: '///', relevance: 0
-                },
-                {
-                  begin: '<!--|-->'
-                },
-                {
-                  begin: '</?', end: '>'
-                }
-              ]
-            }
-          ]
-        }
-      ),
-      // スタイル部 (<style></style>)
+      {
+        begin: '<\\!\\[CDATA\\[', end: '\\]\\]>',
+        relevance: 10
+      },
       {
         className: 'tag',
         /*
@@ -173,7 +107,6 @@ function(hljs) {
           subLanguage: ['css', 'xml']
         }
       },
-      // スクリプト部 (<script></script>)
       {
         className: 'tag',
         // See the comment in the <style tag about the lookahead pattern
@@ -185,7 +118,6 @@ function(hljs) {
           subLanguage: ['actionscript', 'javascript', 'handlebars', 'xml']
         }
       },
-      // HTMLタグ部？
       {
         className: 'tag',
         begin: '</?', end: '/?>',
@@ -197,65 +129,20 @@ function(hljs) {
         ]
       },
 
+      // C# 埋め込み部 @{ ... }
       {
-        className: 'meta',
-        begin: '#', end: '$',
-        keywords: {'meta-keyword': 'if else elif endif define undef warning error line region endregion pragma checksum'}
+        className: 'inline-cs',
+        begin: '@{', contains: recursiveParen('{', '}'),
+        starts: {
+          className: 'inline-cs-end',
+          end: '}', returnEnd: true,
+          subLanguage: 'cs'
+        }
       },
-      STRING,
-      hljs.C_NUMBER_MODE,
-      {
-        beginKeywords: 'class interface', end: /[{;=]/,
-        illegal: /[^\s:]/,
-        contains: [
-          hljs.TITLE_MODE,
-          hljs.C_LINE_COMMENT_MODE,
-          hljs.C_BLOCK_COMMENT_MODE
-        ]
-      },
-      {
-        beginKeywords: 'namespace', end: /[{;=]/,
-        illegal: /[^\s:]/,
-        contains: [
-          hljs.inherit(hljs.TITLE_MODE, {begin: '[a-zA-Z](\\.?\\w)*'}),
-          hljs.C_LINE_COMMENT_MODE,
-          hljs.C_BLOCK_COMMENT_MODE
-        ]
-      },
-      {
-        // Expression keywords prevent 'keyword Name(...)' from being
-        // recognized as a function definition
-        beginKeywords: 'new return throw await',
-        relevance: 0
-      },
-      {
-        className: 'function',
-        begin: '(' + TYPE_IDENT_RE + '\\s+)+' + hljs.IDENT_RE + '\\s*\\(', returnBegin: true, end: /[{;=]/,
-        excludeEnd: true,
-        keywords: KEYWORDS,
-        contains: [
-          {
-            begin: hljs.IDENT_RE + '\\s*\\(', returnBegin: true,
-            contains: [hljs.TITLE_MODE],
-            relevance: 0
-          },
-          {
-            className: 'params',
-            begin: /\(/, end: /\)/,
-            excludeBegin: true,
-            excludeEnd: true,
-            keywords: KEYWORDS,
-            relevance: 0,
-            contains: [
-              STRING,
-              hljs.C_NUMBER_MODE,
-              hljs.C_BLOCK_COMMENT_MODE
-            ]
-          },
-          hljs.C_LINE_COMMENT_MODE,
-          hljs.C_BLOCK_COMMENT_MODE
-        ]
-      }
+      
+      
+      // cshtml コメント {* ... *}
+      hljs.COMMENT('{\\*', '\\*}') // C_BLOCK_COMMENT_MODE の真似
     ]
   };
 }
